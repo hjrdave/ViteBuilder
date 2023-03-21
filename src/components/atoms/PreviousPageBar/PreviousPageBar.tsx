753c201effd34ae0f676d28fab3b0f07c42313f8
David Sanders <dsanders_m633@outlook.com>
2023-03-21 15:06:32 -0400

    added previous page bar comp

--- src/components/atoms/PreviousPageBar/PreviousPageBar.tsx
+++ src/components/atoms/PreviousPageBar/PreviousPageBar.tsx
@@ -12,15 +12,15 @@ export default function PreviousPageBar({onPreviousClick, previousBtnLabel, text
 
     return(
         <>
-            <Row styles={styles.compContainer} style={{backgroundColor: 'black'}}>
-                <Col>
-                    <Button variant='bg-none' onClick={onPreviousClick}>
-                        <i className="fa-regular fa-circle-chevron-left pe-2"></i>
+            <Row className={`py-1 px-3 ${styles.compContainer}`}>
+                <Col sm={3}>
+                    <Button variant='bg-none' onClick={onPreviousClick} className={'text-white ps-0 d-flex align-items-center'}>
+                        <i className={`fa-solid fa-circle-chevron-left pe-2 fs-4 ${styles.prevIcon}`}></i>
                         {previousBtnLabel}
                     </Button>
                 </Col>
-                <Col className={"d-flex align-items-center"}>
-                    <p className={"mb-0"}>{text}</p>
+                <Col className={"d-flex align-items-center justify-content-end"}>
+                    <p className={"mb-0 text-white"}><strong>{text}</strong></p>
                 </Col>
             </Row>
         </>
